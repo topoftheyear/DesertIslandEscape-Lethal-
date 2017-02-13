2559e511dc072c21c7fe1459858c9bc63c512035
topoftheyear <topoftheyear@Gmail.com>
2017-02-13 17:27:22 -0600

    Tile
    Tile

---
+++ tile.js
@@ -0,0 +1,11 @@
+// Tile Object
+function Tile(){
+    var object = {};
+    object.type = function(var name){
+        type = name;
+    }
+    object.picture = function(var img){
+        picture = img;
+    }
+    return object;
+}
