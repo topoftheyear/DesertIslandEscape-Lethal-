2e322a02c815522bd3e3503661ac4ace0857d5ec
topoftheyear <topoftheyear@Gmail.com>
2017-02-21 21:18:39 -0600

    Utter confusion
    ok

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ var mapSize = 17;
 var spawn;
 
 //Side Menu
+var g1;
 var sideMenu;
 
 
@@ -33,13 +34,13 @@ function init(){
     generateMap();
     
     // Side menu
-    var g1 = new createjs.Graphics().beginFill("#d3d3d3").drawRect(960, 0, 256, 960);
+    g1 = new createjs.Graphics().beginFill("#d3d3d3").drawRect(window.width - 256, 0, 256, window.height);
     sideMenu = new createjs.Shape(g1);
     
     // Map movement by mouse added
     gameWorld.addEventListener('mousedown', mouseDnD);
     
-    stage.addChild(gameWorld);
+    //stage.addChild(gameWorld);
     stage.addChild(sideMenu);
     
     createjs.Ticker.setFPS(60);
@@ -108,8 +109,6 @@ function tick(event){
         cnv.width = window.innerHeight - 10 + 256;
         
     }
-    sideMenu.height = cnv.height;
-    sideMenu.x = cnv.width - 256;
     
     stage.update(event);
 }
