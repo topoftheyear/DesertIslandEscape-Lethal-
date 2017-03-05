6204066a879469b72a0c49b6dc02e9a700ccea7b
Icyvexen <icyvexen@gmail.com>
2017-03-05 10:59:50 -0600

    Changes
    Trying to make this work

--- menu.js
+++ menu.js
@@ -1,88 +1,55 @@
-//Button Images
-var leftButton_img = new Image();
-    leftButton_img.crossOrigin = "Anonymous";
-    leftButton_img.src="./Images/LeftButton.png";
+//Declare Empty Buttons
+var leftButton;
+var rightButton;
+var selectButton;
 
-var rightButton_img = new Image();
-    rightButton_img.crossOrigin = "Anonymous";
-    rightButton_img.src="./Images/RightButton.png";
-
-var selectButton_img = new Image();
-    selectButton_img.crossOrigin = "Anonymous";
-    selectButton_img.src="./Images/SelectButton.png";
-
-//Button Shapes
+// leftSprite.addEventListener("mousedown", function(e){
+//     leftSprite.gotoAndPlay("held");
+//     leftSprite.addEventListener('pressup', function(e){
+//         e.target.removeAllEventListeners();
+//         leftSprite.gotoAndPlay("normal");
+//         });
+//     });
 
 
-var leftButtonData = {
-    images:[leftButton_img],
-    frames: {width: 64, height:64},
-    animations: {
-        normal:0,
-        held:1
-    }
-}
-var leftSprite = new createjs.Sprite(new createjs.SpriteSheet(leftButtonData), "normal");
-leftSprite.addEventListener("mousedown", function(e){
-    leftSprite.gotoAndPlay("held");
-    leftSprite.addEventListener('pressup', function(e){
-        e.target.removeAllEventListeners();
-        leftSprite.gotoAndPlay("normal");
-        });
-    });
-leftSprite.x = 300; leftSprite.y = -300;
 
-var rightButtonData = {
-    images:[rightButton_img],
-    frames: {width: 64, height:64},
-    animations: {
-        normal:0,
-        held:1
-    }
-}
-var rightSprite = new createjs.Sprite(new createjs.SpriteSheet(rightButtonData),"normal");
-rightSprite.addEventListener("mousedown", function(e){
-    rightSprite.gotoAndPlay("held");
-    rightSprite.addEventListener('pressup', function(e){
-        e.target.removeAllEventListeners();
-        rightSprite.gotoAndPlay("normal");
-        });
-    });
-rightSprite.x = 500; rightSprite.y = -300;
 
-var selectButtonData = {
-    images:[selectButton_img],
-    frames: {width: 64, height:64},
-    animations: {
-        normal:[0],
-        held:[1]
-    }
-}
-var selectSprite = new createjs.Sprite(new createjs.SpriteSheet(selectButtonData),"normal");
-selectSprite.addEventListener("mousedown", function(e){
-    selectSprite.gotoAndPlay("held");
-    selectSprite.addEventListener('pressup', function(e){
-        e.target.removeAllEventListeners();
-        selectSprite.gotoAndPlay("normal");
-        });
-    });
-selectSprite.x = 400; selectSprite.y = -300;
+// rightSprite.addEventListener("mousedown", function(e){
+//     rightSprite.gotoAndPlay("held");
+//     rightSprite.addEventListener('pressup', function(e){
+//         e.target.removeAllEventListeners();
+//         rightSprite.gotoAndPlay("normal");
+//         });
+//     });
+
+
+
+// selectSprite.addEventListener("mousedown", function(e){
+// selectSprite.gotoAndPlay("held");
+// selectSprite.addEventListener('pressup', function(e){
+//     e.target.removeAllEventListeners();
+//     selectSprite.gotoAndPlay("normal");
+//     });
+// });
+
 
 //Menu container
-var menu = new createjs.Container();
+var menu;
 menu.x = 0;
 menu.y = 0;
+var test_g;
+var test_shape;
 
 //Menu boolean
 var inMenu = false;
 
 //Test stage
-var stage = new createjs.Stage("canvas");
-stage.enableMouseOver(10);
+var stage;
 function load(){
     init();
 }
 function init(){
+    stage = new createjs.Stage("canvas");
     createjs.Ticker.setFPS(60);
     createjs.Ticker.addEventListener("tick", tick);
     startMenu();
@@ -92,19 +59,30 @@ function tick(event){
 
 }
 
-function lButton(event){
-
-}
-
-function rButton(event){
-
-}
-
-function sButton(event) {
-
-}
-
 function startMenu(){
-    menu.addChild(leftSprite,rightSprite,selectSprite);
+    leftButton = new createjs.Sprite(generateSpriteSheet("./Images/LeftButton.png",64,64,0,{normal:0, held:1}), "normal");
+    rightButton = new createjs.Sprite(generateSpriteSheet("./Images/RightButton.png",64,64,0,{normal:0, held:1}),"normal");
+    selectButton = new createjs.Sprite(generateSpriteSheet("./Images/SelectButton.png",64,32,0,{normal:0, held:1}),"normal");
+    
+    test_g = new createjs.Graphic();
+    test_g.beginFill("rgba(255,20,20,0.8");
+    test_g.drawRect("100,100,100,100");
+    test_shape = new createjs.Shape(test_g);
+    
+    menu = new createjs.Container();
+    menu.addChild(test_shape);
     stage.addChild(menu);
+}
+
+function generateSpriteSheet(source, w, h, fps, anime){
+    var img = new Image();
+    img.crossOrigin="Anonymous";
+    img.src = source;
+    var data = {
+        images: [img],
+        frames: {width:w, height:h},
+        framerate: fps,
+        animations: anime
+    }
+    return data;
 }
